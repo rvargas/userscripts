c081b0e4c4779149176a54d6d56f76cf5fd761ff
Rafael Vargas <rv@manoderecha.mx>
2015-06-24 21:20:14 -0500

    Added reverse-comments script

---
+++ reverse-comments.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Reverse comments in inbox
+// @namespace    https://what.cd/
+// @version      1.0
+// @description  This script reverses the order of the comments in the inbox
+// @author       Rafael Vargas
+// @license      GNU GPL v3 <http://www.gnu.org/copyleft/gpl.html>
+// @match        https://what.cd/inbox.php?action=viewconv&id=*
+// ==/UserScript==
+
+var comments = Array.prototype.slice.call(document.querySelectorAll("div.box.vertical_space"));
+var reference = document.querySelector('#messageform');
+
+function move_comments(comment){
+    reference.parentNode.insertBefore(comment, reference.nextSibling);
+}
+
+function main() {
+    comments.forEach(move_comments);
+}
+
+main();
