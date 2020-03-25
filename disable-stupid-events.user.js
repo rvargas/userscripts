1ffa410e88455a0c3825b7383352e3ae81bbc4e1
Rafael Vargas <rvargas@users.noreply.github.com>
2020-03-25 14:55:03 -0600

    Adds El Gráfico

--- disable-stupid-events.user.js
+++ disable-stupid-events.user.js
@@ -9,6 +9,7 @@
 // @match        https://*.eluniversal.com.mx/*
 // @match        https://eluniversal.com.mx/*
 // @match        https://rollingstone.com.mx/*
+// @match        https://*.elgrafico.mx/*
 // @grant        none
 // @updateURL    https://github.com/rvargas/userscripts/raw/master/disable-stupid-events.user.js
 // ==/UserScript==
